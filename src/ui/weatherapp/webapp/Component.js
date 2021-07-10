feb14fae32694a5f30d3280fd05c9f70c4ca1576
LikriT-3 <84126436+LikriT-3@users.noreply.github.com>
2021-07-10 09:00:08 +0300

    +fix

---
+++ src/ui/weatherapp/webapp/Component.js
@@ -0,0 +1,25 @@
+sap.ui.define(["sap/ui/core/UIComponent", "intheme/zweather_app/model/models"], function (UIComponent, models) {
+    "use strict";
+
+    return UIComponent.extend("intheme.zweather_app.Component", {
+        metadata: {
+            manifest: "json",
+        },
+
+        /**
+         * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
+         * @public
+         * @override
+         */
+        init: function () {
+            // call the base component's init function
+            UIComponent.prototype.init.apply(this, arguments);
+
+            // enable routing
+            this.getRouter().initialize();
+
+            // set the device model
+            this.setModel(models.createDeviceModel(), "device");
+        },
+    });
+});
